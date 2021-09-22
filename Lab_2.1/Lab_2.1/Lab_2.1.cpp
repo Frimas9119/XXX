122f26afe7488589b9b596fbce496bcfe55f6156
Frimas9119 <90787948+Frimas9119@users.noreply.github.com>
2021-09-22 17:46:57 +0300

    Add files via upload

---
+++ Lab_2.1/Lab_2.1/Lab_2.1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	double z1;
+	double z2;
+	double x;
+	double y;
+	cout << "X=";
+	cin >> x;
+	cout << "y=";
+	cin >> y;
+	z1 = pow(cos(x), 4) - pow(sin(y), 2) + 0.25 * pow(sin(2*x), 2) - 1;
+	z2 = sin(x + y) * sin(y - x);
+	cout << "Z1=" << z1;
+	cout << "Z2=" << z2;
+}
+
